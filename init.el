51f39e8a954b9843786a5c3077d1ef9e2ec67d99
yuk1kaze <nigetx1016@gmail.com>
2023-04-17 10:38:43 +0900

    evil-mode sucks!

--- init.el
+++ init.el
@@ -187,15 +187,15 @@
   :ensure t)
 
 ;; evil-mode
-(use-package evil
-  :ensure t
-  :config (evil-mode 1))
+;; (use-package evil
+;;   :ensure t
+;;   :config (evil-mode 1))
 
-(use-package key-chord
-  :ensure t
-  :config (setq key-chord-two-keys-delay 0.5)
-  (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
-  (key-chord-mode 1))
+;; (use-package key-chord
+;;   :ensure t
+;;   :config (setq key-chord-two-keys-delay 0.5)
+;;   (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+;;   (key-chord-mode 1))
 
 ;; smart-mode-line
 (use-package smart-mode-line
