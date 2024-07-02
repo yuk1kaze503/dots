2b7175ef59734a15c66a53a927a33b12bef03838
yuk1z4ze <nigetx1016@gmail.com>
2024-07-02 18:25:23 +0900

    fix cl package error.

--- emacs/init.el
+++ emacs/init.el
@@ -37,8 +37,8 @@
     ;; and smaller 80 column windows for smaller displays
     ;; pick whatever numbers make sense for you
     (if (> (x-display-pixel-width) 1280)
-           (add-to-list 'default-frame-alist (cons 'width 200))
-           (add-to-list 'default-frame-alist (cons 'width 60)))
+           (add-to-list 'default-frame-alist (cons 'width 180))
+           (add-to-list 'default-frame-alist (cons 'width 50)))
     ;; for the height, subtract a couple hundred pixels
     ;; from the screen height (for panels, menubars and
     ;; whatnot), then divide by the height of a char to
