a18279af2a2c077a8267a3e4f0e079072b282a47
yuk1z4ze <nigetx1016@gmail.com>
2024-08-22 16:24:19 +0900

    switch rust-mode to rustic

--- emacs/init.el
+++ emacs/init.el
@@ -285,6 +285,27 @@
   :init
   (global-corfu-mode))
 
+(use-package cape
+  ;; Bind prefix keymap providing all Cape commands under a mnemonic key.
+  ;; Press C-c p ? to for help.
+  :bind ("C-c p" . cape-prefix-map) ;; Alternative keys: M-p, M-+, ...
+  ;; Alternatively bind Cape commands individually.
+  ;; :bind (("C-c p d" . cape-dabbrev)
+  ;;        ("C-c p h" . cape-history)
+  ;;        ("C-c p f" . cape-file)
+  ;;        ...)
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  (add-hook 'completion-at-point-functions #'cape-file)
+  (add-hook 'completion-at-point-functions #'cape-elisp-block)
+  ;; (add-hook 'completion-at-point-functions #'cape-history)
+  ;; ...
+)
+
 (use-package corfu-popupinfo
   :ensure nil
   :hook (corfu-mode .corfu-popupinfo-mode))
@@ -640,10 +661,10 @@
 (use-package catppuccin-theme
   :ensure t
   :config
-  ;;(setq catppuccin-flavor 'latte) ;; or 'frappe, 'macchiato, or 'mocha
-  ;;(setq catppuccin-flavor 'frappe)
-  (setq catppuccin-flavor 'macchiato)
-  ;;(setq catppuccin-flavor 'mocha)
+  (setq catppuccin-flavor 'latte) ;; light 
+  ;;(setq catppuccin-flavor 'frappe) ;; dark
+  ;;(setq catppuccin-flavor 'macchiato) ;; dark
+  ;;(setq catppuccin-flavor 'mocha) ;; dark
   ;; (load-theme 'catppuccin t)
   )
 
@@ -832,19 +853,13 @@
                                               'clang-format-buffer t t))))
 
 ;; spell checking
-(use-package flyspell
-  :config
-  (add-hook 'c-mode-hook #'(lambda()
-			    (flyspell-prog-mode)))
-  (add-hook 'c++-mode-hook #'(lambda()
-			      (flyspell-prog-mode)))
-  (add-hook 'go-mode-hook #'(lambda()
-			     (flyspell-prog-mode)))
-  (add-hook 'rust-mode-hook #'(lambda()
-				 (flyspell-prog-mode)))
-  (add-hook 'python-mode-hook #'(lambda()
-				 (flyspell-prog-mode))))
-
+(use-package flymake
+  :hook ((prog-mode
+          conf-mode) . flymake-mode)
+  :init
+  (setq-default flymake-no-changes-timeout 1.0))
+(use-package flymake-collection
+  :hook (after-init . flymake-collection-hook-setup))
 
 (use-package fish-mode
   :ensure t
@@ -861,24 +876,28 @@
   (add-to-list 'eglot-server-programs '(c-mode . ("clangd")))
   (add-to-list 'eglot-server-programs '(c++-mode . ("clangd")))
   (add-to-list 'eglot-server-programs '(go-mode . ("gopls")))
-  (add-to-list 'eglot-server-programs '(rust-mode . ("rust-analyzer")))
+  ;; (add-to-list 'eglot-server-programs
+  ;;              '((rust-mode rust-ts-mode) .
+  ;;                 ("rust-analyzer" :initializationOptions (:check
+  ;;                                    (:command "clippy")))))
   (add-to-list 'eglot-server-programs '(python-mode . ("pylsp"))) ;; python-lsp-server
   (add-to-list 'eglot-server-programs '(kotlin-mode . ("kotlin-language-server")))
   (add-to-list 'eglot-server-programs '(java-mode . ("java-language-server")))
   (add-to-list 'eglot-server-programs '(typescript-mode . ("typescript-language-server" "--stdio")))
   (add-to-list 'eglot-server-programs '(zig-mode . ("zls")))
-  (add-hook 'eglot--managed-mode-hook (lambda() (flymake-mode -1)))
   (add-hook 'c-mode-hook 'eglot-ensure)
   (add-hook 'c++-mode-hook 'eglot-ensure)
   (add-hook 'go-mode-hook 'eglot-ensure)
-  (add-hook 'rust-mode-hook 'eglot-ensure)
+  ;; (add-hook 'rust-mode-hook 'eglot-ensure)
   (add-hook 'python-mode-hook 'eglot-ensure)
   (add-hook 'java-mode-hook 'eglot-ensure)
   (add-hook 'kotlin-mode-hook 'eglot-ensure)
   (add-hook 'typescript-mode-hook 'eglot-ensure)
   (add-hook 'zig-mode-hook 'eglot-ensure)
   (with-eval-after-load "eglot"
-    (add-to-list 'eglot-stay-out-of 'eldoc)) ;; disabled showing minibuffers
+  (add-to-list 'eglot-stay-out-of 'flymake)
+  ;;(add-to-list 'eglot-stay-out-of 'eldoc)
+  ) ;; disabled showing minibuffers
   ;; format on save disabled 03-02-2024
   ;; (add-hook 'c-mode-hook '(lambda() (add-hook 'before-save-hook 'eglot-format-buffer nil t)))
   ;; (add-hook 'c++-mode-hook '(lambda() (add-hook 'before-save-hook 'eglot-format-buffer nil t)))
@@ -931,31 +950,47 @@
 ;; go-mode
 (use-package go-mode
   :ensure t
-  :defer t
-  :mode ("\\.go$" . go-mode)
   :config
-  (setq gofmt-command "goimports")
-  (add-hook 'go-mode-hook (lambda()
-			    (setq-default)
-			    (setq tab-width 4)
-			    (setq standard-indent 4)
-			    (setq ident-tabs-mode nil)))
-  (add-hook 'before-save-hook 'gofmt-before-save))
+  (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+  ;; (setq gofmt-command "goimports")
+  ;; (add-hook 'go-mode-hook (lambda()
+  ;;       		    (setq-default)
+  ;;       		    (setq tab-width 4)
+  ;;       		    (setq standard-indent 4)
+  ;;       		    (setq ident-tabs-mode nil)))
+  ;; (add-hook 'before-save-hook 'gofmt-before-save)
+  )
 
+;; TODO: eglot not working well in rust-mode
+;; ;; rust-mode
+;; (use-package rust-mode
+;;   :ensure t
+;;   :init
+;;   (setq rust-mode-treesitter-derive t)
+;;   ;; :hook (rust-mode . (lambda ()
+;;   ;;                      (setq-local tab-width 4)))
+;;   :config
+;;   (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+;;   ;; (setq rust-format-on-save t)
+;;   )
+;; (use-package cargo-mode
+;;   :ensure t
+;;   :hook
+;;   (rust-mode . cargo-minor-mode)
+;;   :config
+;;   (setq compilation-scroll-output t))
+;; (use-package cargo
+;;   :ensure t)
+;; rustic-mode
+;; 2024-08-22
+(use-package rustic
+  :ensure t
+  :custom
+  (rustic-analyzer-command '("rustup" "run" "stable" "rust-analyzer"))
+  :config
+  (setq rustic-lsp-client 'eglot)
+  )
 
-;; rust-mode
-(use-package rust-mode
-  :ensure t
-  :defer t
-  :hook (rust-mode . (lambda ()
-                       (setq-local tab-width 4)))
-  :mode ("\\.rs$" . rust-mode)
-  :config
-  (setq rust-format-on-save t))
-(use-package cargo-mode
-  :ensure t
-  :config
-  (add-hook 'rust-mode-hook 'cargo-minor-mode))
 
 ;; python
 (use-package elpy
@@ -1091,9 +1126,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("ba430032923a577f4b6d9affd8c03553e13599aa7a33460e00f594b8693115bf" "dcb1cc804b9adca583e4e65755895ba0a66ef82d29464cf89a78b88ddac6ca53" "71c615b0a662a491a5ae8266adfbd8f75dcb0b980f5fecae9dd6494121e4e5a5" "7b8f5bbdc7c316ee62f271acf6bcd0e0b8a272fdffe908f8c920b0ba34871d98" "b40f11c174e7e475508f1e2c1cfca354d37212494c143a494f27239c7d71a294" "90a6f96a4665a6a56e36dec873a15cbedf761c51ec08dd993d6604e32dd45940" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "871b064b53235facde040f6bdfa28d03d9f4b966d8ce28fb1725313731a2bcc8" "ba323a013c25b355eb9a0550541573d535831c557674c8d59b9ac6aa720c21d3" "67f6b0de6f60890db4c799b50c0670545c4234f179f03e757db5d95e99bac332" "046a2b81d13afddae309930ef85d458c4f5d278a69448e5a5261a5c78598e012" "0527c20293f587f79fc1544a2472c8171abcc0fa767074a0d3ebac74793ab117" "2cc1b50120c0d608cc5064eb187bcc22c50390eb091fddfa920bf2639112adb6" "fc608d4c9f476ad1da7f07f7d19cc392ec0fb61f77f7236f2b6b42ae95801a62" "3c3507184cd9b63d58106de248415981dac5facdd22f7266f0b820a9c18f4f5b" "b4c8beafbdaf78e2624f0e4c06b00f40f833c7c8c1d1263f2201f719cb4b4ff9" "2858c51f2d5afa229e836bc303f5c0b7c672a9905a55e947922922b146b44d73" "4dcf06273c9f5f0e01cea95e5f71c3b6ee506f192d75ffda639d737964e2e14e" "603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" "7b303763746ab4ab92fd18d911073aadb1393d36263ba1f04f5e0641e94f6d54" "3de5c795291a145452aeb961b1151e63ef1cb9565e3cdbd10521582b5fd02e9a" "443e2c3c4dd44510f0ea8247b438e834188dc1c6fb80785d83ad3628eadf9294" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "4b287bfbd581ea819e5d7abe139db1fb5ba71ab945cec438c48722bea3ed6689" "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default))
+   '("6b839977baf10a65d9d7aed6076712aa2c97145f45abfa3bad1de9d85bc62a0e" "ba430032923a577f4b6d9affd8c03553e13599aa7a33460e00f594b8693115bf" "dcb1cc804b9adca583e4e65755895ba0a66ef82d29464cf89a78b88ddac6ca53" "71c615b0a662a491a5ae8266adfbd8f75dcb0b980f5fecae9dd6494121e4e5a5" "7b8f5bbdc7c316ee62f271acf6bcd0e0b8a272fdffe908f8c920b0ba34871d98" "b40f11c174e7e475508f1e2c1cfca354d37212494c143a494f27239c7d71a294" "90a6f96a4665a6a56e36dec873a15cbedf761c51ec08dd993d6604e32dd45940" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "871b064b53235facde040f6bdfa28d03d9f4b966d8ce28fb1725313731a2bcc8" "ba323a013c25b355eb9a0550541573d535831c557674c8d59b9ac6aa720c21d3" "67f6b0de6f60890db4c799b50c0670545c4234f179f03e757db5d95e99bac332" "046a2b81d13afddae309930ef85d458c4f5d278a69448e5a5261a5c78598e012" "0527c20293f587f79fc1544a2472c8171abcc0fa767074a0d3ebac74793ab117" "2cc1b50120c0d608cc5064eb187bcc22c50390eb091fddfa920bf2639112adb6" "fc608d4c9f476ad1da7f07f7d19cc392ec0fb61f77f7236f2b6b42ae95801a62" "3c3507184cd9b63d58106de248415981dac5facdd22f7266f0b820a9c18f4f5b" "b4c8beafbdaf78e2624f0e4c06b00f40f833c7c8c1d1263f2201f719cb4b4ff9" "2858c51f2d5afa229e836bc303f5c0b7c672a9905a55e947922922b146b44d73" "4dcf06273c9f5f0e01cea95e5f71c3b6ee506f192d75ffda639d737964e2e14e" "603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" "7b303763746ab4ab92fd18d911073aadb1393d36263ba1f04f5e0641e94f6d54" "3de5c795291a145452aeb961b1151e63ef1cb9565e3cdbd10521582b5fd02e9a" "443e2c3c4dd44510f0ea8247b438e834188dc1c6fb80785d83ad3628eadf9294" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "4b287bfbd581ea819e5d7abe139db1fb5ba71ab945cec438c48722bea3ed6689" "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default))
  '(package-selected-packages
-   '(auto-dark fontaine sqlformat sql-indent highlight-quoted highlight-defined lsp-snippet eglot-x lin treesit-auto puni rg difftastic diff-hl vertico-truncate nerd-icons-corfu org-modern-indent vc-use-package org-modern ox-qmd org-pomodoro ef-themes eglot-signature-eldoc-talkative eldoc-box eglot-tempel consult-eglot imenu-list dashboard undo-fu-session undo-fu tempel-collection tempel embark-consult embark marginalia corfu-prescient vertico-prescient prescient orderless vertico corfu-terminal corfu nerd-icons-dired nerd-icons-completion auto-package-update highlight-indent-guides volatile-highlights beacon doom-modeline mini-echo moody company-anywhere company-statistics zig-mode markdown-mode geiser-guile rust-mode smartparens vscode-dark-plus-theme catppuccin-theme ace-window modus-themes unicode-fonts fish-mode evil-collection editorconfig smart-mode-line-powerline-theme tuareg magit-todos dired tree-sitter-langs tree-sitter cargo-mode rainbow-delimiters hl-todo magit-gitflow arjen-grey-theme exec-path-frome-shell counsel rainbow-mode visual-fill-column virtual-fill-column elpy smart-mode-line-atom-one-dark-theme smart-mode-line smart-jump auto-sudoedit cl-libify lsp-ivy lsp-ui lsp-mode eglot-java kotlin-mode python-mode doom-themes magit sanityinc-tomorrow-day solarized-theme material-theme color-theme-sanityinc-tomorrow key-chord organic-green-theme undo-tree everforest-theme everforest powerline-evil powerline evil vterm cl clang-format monokai-pro-theme nix-mode darkokai-theme darkokai gruvbox-theme yasnippet-snippets yasnippet tide typescript-mode all-the-icons-dired all-the-icons-ibuffer gcmh move-text zenburn-theme darcula-theme darcula zenburn exec-path-from-shell company-box python-black go-mode dracula-theme which-key try use-package))
+   '(rustic cargo flymake-collection cape auto-dark fontaine sqlformat sql-indent highlight-quoted highlight-defined lsp-snippet eglot-x lin treesit-auto puni rg difftastic diff-hl vertico-truncate nerd-icons-corfu org-modern-indent vc-use-package org-modern ox-qmd org-pomodoro ef-themes eglot-signature-eldoc-talkative eldoc-box eglot-tempel consult-eglot imenu-list dashboard undo-fu-session undo-fu tempel-collection tempel embark-consult embark marginalia corfu-prescient vertico-prescient prescient orderless vertico corfu-terminal corfu nerd-icons-dired nerd-icons-completion auto-package-update highlight-indent-guides volatile-highlights beacon doom-modeline mini-echo moody company-anywhere company-statistics zig-mode markdown-mode geiser-guile rust-mode smartparens vscode-dark-plus-theme catppuccin-theme ace-window modus-themes unicode-fonts fish-mode evil-collection editorconfig smart-mode-line-powerline-theme tuareg magit-todos dired tree-sitter-langs tree-sitter cargo-mode rainbow-delimiters hl-todo magit-gitflow arjen-grey-theme exec-path-frome-shell counsel rainbow-mode visual-fill-column virtual-fill-column elpy smart-mode-line-atom-one-dark-theme smart-mode-line smart-jump auto-sudoedit cl-libify lsp-ivy lsp-ui lsp-mode eglot-java kotlin-mode python-mode doom-themes magit sanityinc-tomorrow-day solarized-theme material-theme color-theme-sanityinc-tomorrow key-chord organic-green-theme undo-tree everforest-theme everforest powerline-evil powerline evil vterm cl clang-format monokai-pro-theme nix-mode darkokai-theme darkokai gruvbox-theme yasnippet-snippets yasnippet tide typescript-mode all-the-icons-dired all-the-icons-ibuffer gcmh move-text zenburn-theme darcula-theme darcula zenburn exec-path-from-shell company-box python-black go-mode dracula-theme which-key try use-package))
  '(package-vc-selected-packages
    '((lsp-snippet :vc-backend Git :url "https://github.com/svaante/lsp-snippet")
      (eglot-x :vc-backend Git :url "https://github.com/nemethf/eglot-x")
